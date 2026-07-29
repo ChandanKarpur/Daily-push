8bf6ff59a0a71ab91f13b82297caddd2f7259ce7
ChandanKarpur <chandankarpur2005@gmail.com>
2026-07-29 23:35:00 +0530

    Calculate the total digit of a number

---
+++ DigitCalculate.java
@@ -0,0 +1,21 @@
+public class DigitCalculate {
+    public static int totalDigits(int n) {
+        int temp=0;
+        for(int i=1;i<=n;i++){
+            int number = i;
+            int count = 0;
+
+            while(number > 0) {
+                number /= 10;
+                count++;
+            }
+            temp += count;
+        }
+        return temp;
+    }
+    public static void main(String[] args) {
+        int n = 13; 
+        int totalDigits = totalDigits(n);
+        System.out.println("The total number of digits from 1 to " + n + " is: " + totalDigits);
+    }
+}
