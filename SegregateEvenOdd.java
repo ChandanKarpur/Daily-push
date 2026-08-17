aafb3df34c3e77c60a2bcc1b74f87075e02649ca
ChandanKarpur <chandankarpur2005@gmail.com>
2026-08-17 15:53:33 +0530

    separate even or odd from an array using segregate function

---
+++ SegregateEvenOdd.java
@@ -0,0 +1,29 @@
+public class SegregateEvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        segregateEvenOdd(arr);
+        System.out.println("Array after segregation: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void segregateEvenOdd(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            while (arr[left] % 2 == 0 && left < right) {
+                left++;
+            }
+            while (arr[right] % 2 == 1 && left < right) {
+                right--;
+            }
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
