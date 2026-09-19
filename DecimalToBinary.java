113955897146246c5aa59703f7be330ebd5735e9
ChandanKarpur <chandankarpur2005@gmail.com>
2026-09-19 23:34:43 +0530

    Decimal to Binary number

---
+++ DecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter decimal number: ");
+        int decimal = sc.nextInt();
+        int binary = 0;
+        int place = 1;
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binary = binary + remainder * place;
+            place = place * 10;
+            decimal = decimal / 2;
+        }
+        System.out.println("Binary = " + binary);
+        sc.close();
+    }
+}
